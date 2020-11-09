40997305edce7f28229bb7a50ef5fe3c83eda1de
roms <romain.desajardim@gmail.com>
2020-11-09 16:51:51 +0100

    Day 1

---
+++ symfony/src/Form/PersonType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Person;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PersonType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('gender', ChoiceType::class, [
+                'choices' => [
+                    'Monsieur' => 'Monsieur',
+                    'Madame' => 'Madame',
+                    'Autre' => 'Autre'
+                ]
+            ])
+            ->add('firstname')
+            ->add('lastname')
+            ->add('birthday', DateType::class,
+                [
+                    'years' => range(1980, 2020),
+                    'format' => 'ddMMMMyyyy'
+                ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Person::class,
+        ]);
+    }
+}
